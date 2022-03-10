079d224fbeccf5dc984314c2c0ab5e9bcf34516a
Janku13 <mohammed.mmaljoke@gmail.com>
2022-03-10 00:06:49 -0300

    adding readme file

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,23 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Header from './components/header/header.component';
 import HomePage from './pages/homepage/homepage.component';
 import ProductDetails from './pages/product_details/productDetails';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/:category" element={<HomePage />} />
-        <Route path="/shop/:id" element={<ProductDetails />} />
-      </Routes>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/:category" element={<HomePage />} />
+          <Route path="/shop/:id" element={<ProductDetails />} />
+        </Routes>
+      </div>
+    );
+  }
 }
 
 export default App;
